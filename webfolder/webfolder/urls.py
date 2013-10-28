f1cad2fa8cfc7aabd5f8ec8f32219e67db5ce782
aert <dev.aert@gmail.com>
2013-10-28 18:27:04 +0100

    Added Previous/Next.

--- webfolder/webfolder/urls.py
+++ webfolder/webfolder/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('webfolder.views',
-                       url(r'^$',
-                           TemplateView.as_view(template_name='base.html'),
-                           name='home'),
-                       )
+urlpatterns = patterns(
+    'webfolder.views',
+    url(r'^$',
+        TemplateView.as_view(template_name='webfolder/base.html'),
+        name='home'),
+)
