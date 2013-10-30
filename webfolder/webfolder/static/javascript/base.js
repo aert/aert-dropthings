7bbfcf2422e6083b6acc57d70115f695fd827ef9
aert <dev.aert@gmail.com>
2013-10-30 19:43:00 +0100

    Delete confirmation.

--- webfolder/webfolder/static/javascript/base.js
+++ webfolder/webfolder/static/javascript/base.js
@@ -1,11 +1,17 @@
 $(document)
   .ready(function() {
 
+    // DropDowns
     $('.ui.dropdown')
       .dropdown({
       on: 'hover'
     })
     ;
+
+    // Actions
+    $('.delete_modal')
+     .modal('setting', 'closable', false)
+     .modal('attach events', '.delete_button', 'show');
 
   })
 ;
