7f8a9ded9d3e426b649da8addd9713a1dc0ef6e0
aert <dev.aert@gmail.com>
2013-11-02 19:16:02 +0100

    Remaining: account pages.

--- webfolder/webfolder/urls.py
+++ webfolder/webfolder/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 from .views.files import FilesView
@@ -6,7 +6,12 @@ from .views.files import FilesViewDetail
 from .views.files import FilesViewNewFile
 from .views.friends import FriendsView
 from .views.shares import SharesView
+from .views.account import AccountDetailView
+from .views.account import AccountLogoutView
+from .views.account import AccountLoginView
 
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns(
     'webfolder.views',
@@ -19,4 +24,12 @@ urlpatterns = patterns(
 
     # New...
     url(r'^activity/new-file$', FilesViewNewFile.as_view(), name='files-new'),
+
+    # Account
+    url(r'^account/(?P<pk>\d+)/$', AccountDetailView.as_view(), name='account-detail'),
+    url(r'^logout$', AccountLogoutView.as_view(), name='account-logout'),
+    url(r'^login$', AccountLoginView.as_view(), name='account-login'),
+    
+    # Admin
+    url(r'^admin', include(admin.site.urls)),
 )
