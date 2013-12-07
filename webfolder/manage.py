fe047b0fcbf8694d34cb834727eb5edf31d2e981
aert <dev.aert@gmail.com>
2013-12-07 22:12:15 +0100

    Added favicon.

--- webfolder/manage.py
+++ webfolder/manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     setting_module = "webfolder.webui.settings.main"
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", setting_module)
+    os.environ["DJANGO_SETTINGS_MODULE"] = setting_module
 
     from django.core.management import execute_from_command_line
 
